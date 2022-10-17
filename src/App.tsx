331ffe5dc75254547f9d958ca6f8e7f717e6f06b
rifatsaown <rifatsaown0@gmail.com>
2022-10-17 06:50:24 +0600

    render

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import PostGet from './components/PostsGet/PostGet';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World </h1>
+      <PostGet/>
     </div>
   );
 }
---
+++ src/shared/types.d.ts
@@ -0,0 +1,5 @@
+export interface iPost {
+    title: string;
+    body: string;
+    userId: number;
+}
