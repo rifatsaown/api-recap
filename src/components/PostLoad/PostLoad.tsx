403c8c9318857530a6854247903115c9b39c8f35
rifatsaown <rifatsaown0@gmail.com>
2022-10-17 21:04:18 +0600

    final

--- src/components/PostLoad/PostLoad.tsx
+++ src/components/PostLoad/PostLoad.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { iPost } from '../../shared/types';
+import React from "react";
+import { iPost } from "../../shared/types";
 
-const PostLoad = (props:Object) => {
-    const {title,body, userId} = props as iPost;
-    
-    return (
-        <div style={{background :"lightgray" , padding : "2rem" ,margin: "2rem"}}>
-            <h3>Title : {title} </h3>
-            <p>UserId : {userId} </p>
-            <p>Body : {body} </p>
-            
-        </div>
-    );
+const PostLoad = (props: Object) => {
+  const { title, body, userId } = props as iPost;
+
+  return (
+    <div style={{ background: "lightgray", padding: "2rem", margin: "2rem" }}>
+      <h3>Title : {title} </h3>
+      <p>User Id : {userId} </p>
+      <p>Body : {body} </p>
+    </div>
+  );
 };
 
 export default PostLoad;
